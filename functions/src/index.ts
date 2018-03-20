193d83d03fa9b35c5d159565d71bcb824813e8c5
Cody Winton <cody.t.winton@gmail.com>
2018-03-20 12:29:34 -0500

    Updated project to enable better null checking

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,47 @@
+// Interfaces
+
+interface UserObject {
+  firstName?: string
+  lastName?: string
+  email?: string
+  photoUrl?: string
+  createdAt?: string
+}
+
+// Imports
+
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
+// Setup
+
 admin.initializeApp(functions.config().firebase)
+
+// Functions
 
 exports.onCreateUser = functions.auth.user().onCreate(event => {
   const user = event.data
-  const userObject = {
-    displayName: user.displayName,
-    email: user.email,
-    photoUrl: user.photoURL,
-    createdAt: user.metadata.creationTime,
+  const userObject: UserObject = {}
+
+  if (user.email) {
+    userObject.email = user.email
+  }
+
+  if (user.photoURL) {
+    userObject.photoUrl = user.photoURL
+  }
+
+  if (user.displayName && user.displayName.length > 0) {
+    const names = user.displayName.split(' ')
+    userObject.firstName = names[0]
+
+    if (names.length > 1) {
+      userObject.lastName = names[names.length - 1]
+    }
+  }
+
+  if (user.metadata.creationTime) {
+    userObject.createdAt = user.metadata.creationTime
   }
 
   return admin
