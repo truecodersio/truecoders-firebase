1783691e4d57ce59d6461e55e8b387946eefe3bc
Cody Winton <cody.t.winton@gmail.com>
2018-03-20 12:04:31 -0500

    Added cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,27 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+admin.initializeApp(functions.config().firebase)
+
+exports.onCreateUser = functions.auth.user().onCreate(async event => {
+  const user = event.data
+  const userObject = {
+    displayName: user.displayName,
+    email: user.email,
+    photoUrl: user.photoURL,
+    createdAt: user.metadata.creationTime,
+  }
+
+  return await admin
+    .database()
+    .ref(`users/${user.uid}`)
+    .set(userObject)
+})
+
+exports.onDeleteUser = functions.auth.user().onDelete(async event => {
+  const user = event.data
+  return await admin
+    .database()
+    .ref(`users/${user.uid}`)
+    .remove()
+})
